396b8b6fa67f97a769738c41cf22a337b8d5ef9d
Cassandrea <cassssandrea@gmail.com>
2017-05-09 11:38:12 -0600

    functional example added to GenericRequestHandler

--- src/main/java/com/apis/GenericRequestHandler.java
+++ src/main/java/com/apis/GenericRequestHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,23 +13,25 @@ import org.springframework.web.client.RestTemplate;
  * https://github.com/mouryar/spring-boot-generic-rest-client-sample
  * @author cass
  */
+@Service
 public class GenericRequestHandler<T, V>{
 
     //This auto-creates an instance of RestTemplate which is Spring's central class for client-side HTTP access
     @Autowired
-    RestTemplate restTemplate;
-
+    private RestTemplate restTemplate;
 
     /**
      * The callAPI method is used to make client-side API calls that return a response. It takes the API info provided
      * in a ThirdPartyRequest object and uses an instance of HttpEntity and ResponseEntity to return an object
-     * of the specific type(V)
+     * of the specific type(V).
+     * It is overloaded several times for use with or without headers and body.
      *
      * @param thirdPartyRequest - you'll need to create an instance of this class that contains the specific API details
      * @param genericClass - the class you want the response returned to, which is passed to the restTemplate.exchange
      *                     method
      * @return - the body of the responseEntity - this is a object of the genericClass specified above
      */
+    //no headers or body
     public V callAPI(ThirdPartyRequest thirdPartyRequest, Class<V> genericClass){
 
         HttpEntity<T> entity = new HttpEntity<T>(null,null);
@@ -36,15 +39,9 @@ public class GenericRequestHandler<T, V>{
                 entity, genericClass);
         return response.getBody();
     }
-    /**
-     * Overloaded callAPI method that is the same as above, but adds a requestBody parameter
-     *
-     * @param thirdPartyRequest - you'll need to create an instance of this class that contains the specific API details
-     * @param requestBody - body of the request that gets passed to the HttpEntity constructor
-     * @param genericClass - the class you want the response returned to, which is passed to the restTemplate.exchange
-     *                     method
-     * @return - the body of the responseEntity - this is a object of the genericClass specified above
+    /** @param requestBody - body of the request that gets passed to the HttpEntity constructor
      */
+    // body, no headers
     public V callAPI(ThirdPartyRequest thirdPartyRequest, T requestBody, Class<V> genericClass){
 
         HttpEntity<T> entity = new HttpEntity<T>(requestBody);
@@ -53,16 +50,17 @@ public class GenericRequestHandler<T, V>{
         return response.getBody();
         }
 
-    /**
-     * Overloaded callAPI method that is the same as above, but allows headers parameter
-     *
-     * @param thirdPartyRequest - you'll need to create an instance of this class that contains the specific API details
-     * @param requestBody - body of the request that gets passed to the HttpEntity constructor
-     * @param genericClass - the class you want the response returned to, which is passed to the restTemplate.exchange
-     *                     method
-     * @param headers - an instance of HttpHeaders that consists of a name and value
-     * @return the body of the responseEntity - this is a object of the genericClass specified above
+    /** @param headers - an instance of HttpHeaders that consists of a name and value
      */
+    // headers, no body
+    public V callAPI(ThirdPartyRequest thirdPartyRequest, Class<V> genericClass, HttpHeaders headers){
+
+        HttpEntity<T> entity = new HttpEntity<T>(headers);
+        ResponseEntity<V> response = restTemplate.exchange(thirdPartyRequest.getUrl(), thirdPartyRequest.getRequestType(),
+                entity, genericClass);
+        return response.getBody();
+    }
+    // body and headers
     public V callAPI(ThirdPartyRequest thirdPartyRequest, T requestBody, Class<V> genericClass, HttpHeaders headers){
 
         HttpEntity<T> entity = new HttpEntity<T>(requestBody, headers);
@@ -72,22 +70,33 @@ public class GenericRequestHandler<T, V>{
     }
 
     /**
-     * These are examples of how to use the generic request handler.
-     * I can use these for testing once I have an API and POJOs, then use this format for all other 3rd party calls.
+     * Functional example resource class using the generic request handler. This could also be used outside of a
+     * resource class by just calling the methods, beerGet and beerPost. Note the @RestController annotation on the
+     * class and the @Autowired injection of the GenericRequestHandler.
      */
-    public void exampleGet() {
-        new GenericRequestHandler<String, String>()
-                .callAPI(
-                        new ThirdPartyRequest("http://exampleWebsite", HttpMethod.GET), String.class);
-    }
-    public void examplePost(){
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Basic-Auth", "examplekey");
-        new GenericRequestHandler<String, String>()
-                .callAPI(
-                        new ThirdPartyRequest("http://exampleWebsite", HttpMethod.POST),
-                        "Body of the request", String.class, headers);
-    }
+//    @RestController
+//    public class BeerResource {
+//
+//        @Autowired
+//        private GenericRequestHandler handler;
+//
+//        @RequestMapping("/beer")
+//        public Object beerGet() {
+//            ThirdPartyRequest callBeer = new ThirdPartyRequest(
+//                    "http://api.brewerydb.com/v2/beer/random?key=47dd1ef7d420f2f8b23998fabfbc336e", HttpMethod.GET);
+//            return handler.callAPI(callBeer, Beer.class);
+//        }
+//
+//        //This won't actually work as we can't post to the beer API
+//        @RequestMapping(method = RequestMethod.POST, value = "/beer")
+//        public Object beerPost(){
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.add("Basic-Auth", "examplekey");
+//            ThirdPartyRequest callBeer = new ThirdPartyRequest(
+//                    "http://api.brewerydb.com/v2/beer/random?key=47dd1ef7d420f2f8b23998fabfbc336e", HttpMethod.POST);
+//            return handler.callAPI(callBeer,"Body of the request", String.class, headers);
+//        }
+//    }
 }
 
 
