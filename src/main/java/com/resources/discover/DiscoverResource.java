f1e95440725edd2939638ec9154e9a3ab5533fa5
Cassandrea <cassssandrea@gmail.com>
2017-06-01 13:02:35 -0600

    updated comments

--- src/main/java/com/resources/discover/DiscoverResource.java
+++ src/main/java/com/resources/discover/DiscoverResource.java
@@ -17,23 +17,33 @@ public class DiscoverResource {
     @Autowired
     private DiscoverService service;
 
-    // Create
-    // this will accept a post request with a body containing a trip object
-    // the trip object should contain the details you'd like in a trip and this will return matching destinations
-    // and links to book (using "/discover/book")
+    /** This will accept a post request with a body containing a trip object
+     * the trip object should contain the details you'd like in a trip and this will return matching destinations
+     *
+     * I still need to figure out how to also return the information required to then book a trip
+     *
+     * @param tripRequest
+     * @return APIResponse with a body containing matching destinations
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/")
     public APIResponse discover(@RequestBody Trip tripRequest){
         //map parameters in the body to a trip object and pass to the service
         return service.discoverDestinations(tripRequest);
     }
 
-    //This might be redundant depending on how the booking resource looks
-    //This needs to accept url parameters with a tripId or it could just accept a trip object, your choice
-    //It will then send the necessary info to the booking service(not sure if that exists yet or will even be created)
+
+    /**
+     * This is a placeholder and it might be redundant or need to be totally different
+     * This could accept url parameters with a tripId or it could just accept a trip object, your choice
+     * It would then send the necessary info to the booking service
+     * (not sure if that exists yet or will even be created - otherwise it would take the place of the booking service)
+     *
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/book")
     public APIResponse discoverBook(){
         Trip tripRequest = new Trip();//placeholder - you'd actually just look up a trip by id, or accept a trip in the
         // body of the request
-        return service.discover(tripRequest);
+        return service.discoverBook(tripRequest);
     }
 }
